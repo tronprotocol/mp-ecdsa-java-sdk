310a1a6ef74a517b021af1692c1f3dff21750a73
federico.zhen <federico.zhen@tron.network>
2020-09-03 14:33:09 +0800

    add the sign jni

--- cpp/src/LibmpecdsaJNIImpl.cpp
+++ cpp/src/LibmpecdsaJNIImpl.cpp
@@ -199,3 +199,336 @@ JNIEXPORT jstring JNICALL
 
     return result;
 }
+
+/*
+ * Class:     org_tron_common_tss_Libmpecdsa_LibmpecdsaJNI
+ * Method:    libmpecdsaSignCtxInit
+ * Signature: (II)J
+ */
+JNIEXPORT jlong JNICALL
+Java_org_tron_common_tss_Libmpecdsa_00024LibmpecdsaJNI_libmpecdsaSignCtxInit
+    (JNIEnv * env, jobject, jint n, jint t) {
+//    void *libmpecdsa_sign_ctx_init(
+//            int32_t party_total,  // n
+//            int32_t threshold    //  t
+//    );
+
+    return (jlong) libmpecdsa_sign_ctx_init((size_t) n, (size_t) t);
+}
+
+/*
+ * Class:     org_tron_common_tss_Libmpecdsa_LibmpecdsaJNI
+ * Method:    libmpecdsaSignCtxFree
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL
+Java_org_tron_common_tss_Libmpecdsa_00024LibmpecdsaJNI_libmpecdsaSignCtxFree
+    (JNIEnv * env, jobject, jlong ctx) {
+//    void *libmpecdsa_sign_ctx_free(void *);
+    libmpecdsa_sign_ctx_free((void *) ctx);
+}
+
+/*
+ * Class:     org_tron_common_tss_Libmpecdsa_LibmpecdsaJNI
+ * Method:    libmpecdsaSignRound1
+ * Signature: (JLjava/lang/String;[II[I[I)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_tron_common_tss_Libmpecdsa_00024LibmpecdsaJNI_libmpecdsaSignRound1
+    (JNIEnv * env, jobject, jlong ctx, jstring keygen_result, jintArray signers, jint signers_num,
+    jintArray commit_length, jintArray m_a_k_length) {
+//char *libmpecdsa_sign_round1(
+//        void *ctx,
+//        char *keygen_result,      //the keygen result
+//        const int32_t *signers,         //the parties envolving in generating the signature
+//        int32_t signers_num,      //the number of signers, must be larger that threshold (t)
+//        int32_t *commit_length,   // the length of commit in the returned value, size = 1
+//        int32_t *m_a_k_length       // the length of m_a_k in the returned value, size = 1
+//);
+    char *k = (char*) env->GetStringUTFChars(keygen_result, nullptr);
+    const jint *s = env->GetIntArrayElements(signers, nullptr);
+
+    jint *c = env->GetIntArrayElements(commit_length, nullptr);
+    jint *m = env->GetIntArrayElements(m_a_k_length, nullptr);
+
+    if (k == NULL || s == NULL || c == NULL || m == NULL) {
+        return NULL;
+    }
+
+    char *r = libmpecdsa_sign_round1((void *)ctx, k, s, (size_t) signers_num, c, m);
+    jstring result = env->NewStringUTF(r);
+
+    env->ReleaseStringUTFChars(keygen_result, k);
+    env->ReleaseIntArrayElements(signers, (jint *)s, 0);
+    env->ReleaseIntArrayElements(commit_length, c, 0);
+    env->ReleaseIntArrayElements(m_a_k_length, m, 0);
+
+    return result;
+}
+
+
+/*
+ * Class:     org_tron_common_tss_Libmpecdsa_LibmpecdsaJNI
+ * Method:    libmpecdsaSignRound2
+ * Signature: (JLjava/lang/String;[ILjava/lang/String;[I[I[I)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_tron_common_tss_Libmpecdsa_00024LibmpecdsaJNI_libmpecdsaSignRound2
+    (JNIEnv * env, jobject, jlong ctx, jstring commits, jintArray commits_length, jstring m_a_ks,
+    jintArray m_a_ks_length, jintArray m_b_gamma_length, jintArray m_b_wi_length) {
+//char *libmpecdsa_sign_round2(
+//        void *ctx,
+//        char *commits,                //size = signers_num
+//        const int32_t *commits_length,      // size = signers_num
+//        char *m_a_ks,                 // size = signers_num
+//        const int32_t *m_a_ks_length,       // size = signers_num
+//        int32_t *m_b_gamma_length,    // size = signers_num - 1
+//        int32_t *m_b_wi_length        // size = signers_num - 1
+//);
+    char *c = (char*) env->GetStringUTFChars(commits, nullptr);
+    const jint *cl = env->GetIntArrayElements(commits_length, nullptr);
+    char *m = (char*) env->GetStringUTFChars(m_a_ks, nullptr);
+    const jint *ml = env->GetIntArrayElements(m_a_ks_length, nullptr);
+
+    jint *mb = env->GetIntArrayElements(m_b_gamma_length, nullptr);
+    jint *mw = env->GetIntArrayElements(m_b_wi_length, nullptr);
+
+    if (c == NULL || cl == NULL || m == NULL || ml == NULL || mb == NULL || mw == NULL) {
+        return NULL;
+    }
+
+    char *r = libmpecdsa_sign_round2((void *)ctx, c, cl, m, ml, mb, mw);
+    jstring result = env->NewStringUTF(r);
+
+    env->ReleaseStringUTFChars(commits, c);
+    env->ReleaseIntArrayElements(commits_length, (jint *)cl, 0);
+    env->ReleaseStringUTFChars(m_a_ks, m);
+    env->ReleaseIntArrayElements(m_a_ks_length, (jint*)ml, 0);
+    env->ReleaseIntArrayElements(m_b_gamma_length, mb, 0);
+    env->ReleaseIntArrayElements(m_b_wi_length, mw, 0);
+
+    return result;
+}
+
+/*
+ * Class:     org_tron_common_tss_Libmpecdsa_LibmpecdsaJNI
+ * Method:    libmpecdsaSignRound3
+ * Signature: (JLjava/lang/String;[ILjava/lang/String;[I)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_tron_common_tss_Libmpecdsa_00024LibmpecdsaJNI_libmpecdsaSignRound3
+    (JNIEnv * env, jobject, jlong ctx, jstring m_b_gamma_rec, jintArray m_b_gamma_length,
+    jstring m_b_wi_rec, jintArray m_b_wi_length) {
+//char *libmpecdsa_sign_round3(
+//       void *ctx,
+//       char *m_b_gamma_rec,         // size = signers_num - 1
+//       const int32_t *m_b_gamma_length,   // size = signers_num - 1
+//       char *m_b_wi_rec,            // size = signers_num - 1
+//       const int32_t *m_b_wi_rec_length   // size = signers_num - 1
+//);
+    char *mg = (char*) env->GetStringUTFChars(m_b_gamma_rec, nullptr);
+    const jint *mgl = env->GetIntArrayElements(m_b_gamma_length, nullptr);
+    char *mw = (char*) env->GetStringUTFChars(m_b_wi_rec, nullptr);
+    const jint *mwl = env->GetIntArrayElements(m_b_wi_length, nullptr);
+
+    if (mg == NULL || mgl == NULL || mw == NULL || mwl == NULL) {
+        return NULL;
+    }
+
+    char *r = libmpecdsa_sign_round3((void *)ctx, mg, mgl, mw, mwl);
+    jstring result = env->NewStringUTF(r);
+
+    env->ReleaseStringUTFChars(m_b_wi_rec, mg);
+    env->ReleaseIntArrayElements(m_b_gamma_length, (jint *)mgl, 0);
+    env->ReleaseStringUTFChars(m_b_wi_rec, mw);
+    env->ReleaseIntArrayElements(m_b_wi_length, (jint *)mwl, 0);
+
+    return result;
+}
+
+/*
+ * Class:     org_tron_common_tss_Libmpecdsa_LibmpecdsaJNI
+ * Method:    libmpecdsaSignRound4
+ * Signature: (JLjava/lang/String;[I)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_tron_common_tss_Libmpecdsa_00024LibmpecdsaJNI_libmpecdsaSignRound4
+    (JNIEnv * env, jobject, jlong ctx, jstring delta_i_rec, jintArray delta_i_length) {
+//char *libmpecdsa_sign_round4(
+//       void *ctx,
+//       char *delta_i_rec,    // size = signers_num
+//       const int32_t *delta_i_length   // size = signers_nun
+//);
+    char *d = (char*) env->GetStringUTFChars(delta_i_rec, nullptr);
+    const jint *dl = env->GetIntArrayElements(delta_i_length, nullptr);
+
+    if (d == NULL || dl == NULL) {
+        return NULL;
+    }
+
+    char *r = libmpecdsa_sign_round4((void *)ctx, d, dl);
+    jstring result = env->NewStringUTF(r);
+
+    env->ReleaseStringUTFChars(delta_i_rec, d);
+    env->ReleaseIntArrayElements(delta_i_length, (jint *)dl, 0);
+
+    return result;
+}
+
+/*
+ * Class:     org_tron_common_tss_Libmpecdsa_LibmpecdsaJNI
+ * Method:    libmpecdsaSignRound5
+ * Signature: (JLjava/lang/String;[I[I)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_tron_common_tss_Libmpecdsa_00024LibmpecdsaJNI_libmpecdsaSignRound5
+    (JNIEnv * env, jobject, jlong ctx, jstring decommit_rec, jintArray decommit_length,
+      jintArray r_dash_proof_length) {
+//char *libmpecdsa_sign_round5(
+//       void *ctx,
+//       char *decommit_rec,         // size = signers_num
+//       const int32_t *decommit_length,     // size = signers_num
+//       int32_t *r_dash_proof_length   // size = 3
+//);
+    char *d = (char*) env->GetStringUTFChars(decommit_rec, nullptr);
+    const jint *dl = env->GetIntArrayElements(decommit_length, nullptr);
+    jint *rl = env->GetIntArrayElements(r_dash_proof_length, nullptr);
+
+    if (d == NULL || dl == NULL || rl == NULL) {
+        return NULL;
+    }
+
+    char *r = libmpecdsa_sign_round5((void *)ctx, d, dl, rl);
+    jstring result = env->NewStringUTF(r);
+
+    env->ReleaseStringUTFChars(decommit_rec, d);
+    env->ReleaseIntArrayElements(decommit_length, (jint *)dl, 0);
+    env->ReleaseIntArrayElements(r_dash_proof_length, rl, 0);
+
+    return result;
+}
+
+/*
+ * Class:     org_tron_common_tss_Libmpecdsa_LibmpecdsaJNI
+ * Method:    libmpecdsaSignRound6
+ * Signature: (JLjava/lang/String;[ILjava/lang/String;[ILjava/lang/String;[I[I)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_tron_common_tss_Libmpecdsa_00024LibmpecdsaJNI_libmpecdsaSignRound6
+    (JNIEnv * env, jobject, jlong ctx, jstring R_rec, jintArray R_length, jstring R_dash_rec,
+      jintArray R_dash_length, jstring phase5_proof_rec, jintArray phase5_proof_length,
+      jintArray S_proof_T_length) {
+//char *libmpecdsa_sign_round6(
+//       void *ctx,
+//       char *R_rec,           // size = signers_num
+//       const int32_t *R_length,     // size = signers_num
+//       char *R_dash_rec,        // size = signers_num
+//       const int32_t *R_dash_length,       // size = signers_num
+//       char *phase5_proof_rec,       // size = signers_num
+//       const int32_t *phase5_proof_length,  // size = signers_num
+//       int32_t *S_proof_T_length     // size =  3
+//);
+    char *rr = (char*) env->GetStringUTFChars(R_rec, nullptr);
+    const jint *rrl = env->GetIntArrayElements(R_length, nullptr);
+    char *rd = (char*) env->GetStringUTFChars(R_dash_rec, nullptr);
+    const jint *rdl = env->GetIntArrayElements(R_dash_length, nullptr);
+    char *p = (char*) env->GetStringUTFChars(phase5_proof_rec, nullptr);
+    const jint *pl = env->GetIntArrayElements(phase5_proof_length, nullptr);
+    jint *s = env->GetIntArrayElements(S_proof_T_length, nullptr);
+
+    if (rr == NULL || rrl == NULL || rd == NULL || rdl == NULL ||
+        p == NULL || pl == NULL || s == NULL) {
+        return NULL;
+    }
+
+    char *r = libmpecdsa_sign_round6((void *)ctx, rr, rrl, rd, rdl, p, pl, s);
+    jstring result = env->NewStringUTF(r);
+
+    env->ReleaseStringUTFChars(R_rec, rr);
+    env->ReleaseIntArrayElements(R_length, (jint *)rrl, 0);
+    env->ReleaseStringUTFChars(R_dash_rec, rd);
+    env->ReleaseIntArrayElements(R_dash_length, (jint *)rdl, 0);
+    env->ReleaseStringUTFChars(phase5_proof_rec, p);
+    env->ReleaseIntArrayElements(phase5_proof_length, (jint *)pl, 0);
+    env->ReleaseIntArrayElements(S_proof_T_length, s, 0);
+
+    return result;
+}
+
+/*
+ * Class:     org_tron_common_tss_Libmpecdsa_LibmpecdsaJNI
+ * Method:    libmpecdsaSignRound7
+ * Signature: (JLjava/lang/String;[ILjava/lang/String;[ILjava/lang/String;[I[ILjava/lang/String;)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_tron_common_tss_Libmpecdsa_00024LibmpecdsaJNI_libmpecdsaSignRound7
+    (JNIEnv * env, jobject, jlong ctx, jstring S_rec, jintArray S_length, jstring homo_proof_rec,
+     jintArray homo_proof_length, jstring T_i_rec, jintArray T_i_length, jintArray sig_s_i_length,
+     jstring message) {
+//char *libmpecdsa_sign_round7(
+//       void *ctx,
+//       char *S_rec,           // size = signers_num
+//       const int32_t *S_length,          // size = singers_num
+//       char *homo_proof_rec,          //size = signers_num
+//       const int32_t *homo_proof_length,    //size = signers_num
+//       char *T_i_rec,                 // size = signers_num
+//       const int32_t *T_i_length,          // size = signers_num
+//       int32_t *sig_s_i_length,      // size = 2
+//       char *message               // the message to be signed
+//);
+    char *s = (char*) env->GetStringUTFChars(S_rec, nullptr);
+    const jint *sl = env->GetIntArrayElements(S_length, nullptr);
+    char *h = (char*) env->GetStringUTFChars(homo_proof_rec, nullptr);
+    const jint *hl = env->GetIntArrayElements(homo_proof_length, nullptr);
+    char *t = (char*) env->GetStringUTFChars(T_i_rec, nullptr);
+    const jint *tl = env->GetIntArrayElements(T_i_length, nullptr);
+    jint *ss = env->GetIntArrayElements(sig_s_i_length, nullptr);
+    char *m = (char*) env->GetStringUTFChars(message, nullptr);
+
+    if (s == NULL || sl == NULL || h == NULL || hl == NULL ||
+        t == NULL || tl == NULL || ss == NULL || m == NULL) {
+        return NULL;
+    }
+
+    char *r = libmpecdsa_sign_round7((void *)ctx, s, sl, h, hl, t, tl, ss, m);
+    jstring result = env->NewStringUTF(r);
+
+    env->ReleaseStringUTFChars(S_rec, s);
+    env->ReleaseIntArrayElements(S_length, (jint *)sl, 0);
+    env->ReleaseStringUTFChars(homo_proof_rec, h);
+    env->ReleaseIntArrayElements(homo_proof_length, (jint *)hl, 0);
+    env->ReleaseStringUTFChars(T_i_rec, t);
+    env->ReleaseIntArrayElements(T_i_length, (jint *)tl, 0);
+    env->ReleaseIntArrayElements(sig_s_i_length, ss, 0);
+    env->ReleaseStringUTFChars(message, m);
+
+    return result;
+}
+
+/*
+ * Class:     org_tron_common_tss_Libmpecdsa_LibmpecdsaJNI
+ * Method:    libmpecdsaSignRound8
+ * Signature: (JLjava/lang/String;[ILjava/lang/String;[I)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_tron_common_tss_Libmpecdsa_00024LibmpecdsaJNI_libmpecdsaSignRound8
+    (JNIEnv * env, jobject, jlong ctx, jstring local_sig_rec, jintArray local_sig_length,
+    jstring s_i_rec, jintArray s_i_length) {
+//char *libmpecdsa_sign_round8(
+//      void *ctx,
+//      char *local_sig_rec,           // size = signers_num
+//      const int32_t *local_sig_length,     // size = signers_num
+//      char *s_i_rec,                 //  size = signers_num
+//      const int32_t *s_i_length            // size = signers_num
+//);
+    char *l = (char*) env->GetStringUTFChars(local_sig_rec, nullptr);
+    const jint *ll = env->GetIntArrayElements(local_sig_length, nullptr);
+    char *s = (char*) env->GetStringUTFChars(s_i_rec, nullptr);
+    const jint *sl = env->GetIntArrayElements(s_i_length, nullptr);
+
+    if (l == NULL || ll == NULL || s == NULL || sl == NULL) {
+        return NULL;
+    }
+
+    char *r = libmpecdsa_sign_round8((void *)ctx, l, ll, s, sl);
+    jstring result = env->NewStringUTF(r);
+
+    env->ReleaseStringUTFChars(local_sig_rec, l);
+    env->ReleaseIntArrayElements(local_sig_length, (jint *)ll, 0);
+    env->ReleaseStringUTFChars(s_i_rec, s);
+    env->ReleaseIntArrayElements(s_i_length, (jint *)sl, 0);
+
+    return result;
+}
--- src/main/java/org/tron/common/tss/Libmpecdsa.java
+++ src/main/java/org/tron/common/tss/Libmpecdsa.java
@@ -36,6 +36,62 @@ class Libmpecdsa {
     return INSTANCE.libmpecdsaKeygenRound5(ctx, dlogProofs, dlofProofLength, resultLength);
   }
 
+  public long libmpecdsaSignCtxInit(int n, int t) {
+    return INSTANCE.libmpecdsaSignCtxInit(n, t);
+  }
+
+  public void libmpecdsaSignCtxFree(long ctx) {
+    INSTANCE.libmpecdsaSignCtxFree(ctx);
+  }
+
+  public String libmpecdsaSignRound1(long ctx, String keygenResult, int[] signers,
+      int signersNum, int[] commitLength, int[] mAKLength) {
+    return INSTANCE
+        .libmpecdsaSignRound1(ctx, keygenResult, signers, signersNum, commitLength, mAKLength);
+  }
+
+  public String libmpecdsaSignRound2(long ctx, String commits, int[] commitsLength,
+      String mAKs, int[] mAKsLength, int[] mBGammaLength, int[] mBWiLength) {
+    return INSTANCE
+        .libmpecdsaSignRound2(ctx, commits, commitsLength, mAKs, mAKsLength, mBGammaLength,
+            mBWiLength);
+  }
+
+  public String libmpecdsaSignRound3(long ctx, String mBGammaRec, int[] mBGammaLength,
+      String mBWiRec, int[] mBWiRecLengh) {
+    return INSTANCE.libmpecdsaSignRound3(ctx, mBGammaRec, mBGammaLength, mBWiRec, mBWiRecLengh);
+  }
+
+  public String libmpecdsaSignRound4(long ctx, String deltaIRec, int[] deltaILength) {
+    return INSTANCE.libmpecdsaSignRound4(ctx, deltaIRec, deltaILength);
+  }
+
+  public String libmpecdsaSignRound5(long ctx, String decommitRec, int[] decommitLength,
+      int[] rDashProofLength) {
+    return INSTANCE.libmpecdsaSignRound5(ctx, decommitRec, decommitLength, rDashProofLength);
+  }
+
+  public String libmpecdsaSignRound6(long ctx, String rRec, int[] rLength,
+      String rDash, int[] rDashLength, String phase5ProofRec, int[] phase5ProofLength,
+      int[] sProofTLength) {
+    return INSTANCE.libmpecdsaSignRound6(ctx, rRec, rLength, rDash, rDashLength, phase5ProofRec,
+        phase5ProofLength, sProofTLength);
+  }
+
+  public String libmpecdsaSignRound7(long ctx, String sRec, int[] sLength,
+      String homoProofRec, int[] homoProofLength, String tiRec, int[] tiLength,
+      int[] sigSiLength, String message) {
+    return INSTANCE
+        .libmpecdsaSignRound7(ctx, sRec, sLength, homoProofRec, homoProofLength, tiRec, tiLength,
+            sigSiLength, message);
+  }
+
+  public String libmpecdsaSignRound8(long ctx, String localSigRec, int[] localSigLength,
+      String siRec, int[] siLength) {
+    return INSTANCE.libmpecdsaSignRound8(ctx, localSigRec, localSigLength, siRec, siLength);
+  }
+
+
   private static class LibmpecdsaJNI {
 
     private native long libmpecdsaKeygenCtxInit(int i, int n, int t);
@@ -55,6 +111,35 @@ class Libmpecdsa {
 
     private native String libmpecdsaKeygenRound5(long ctx, String dlogProofs,
         int[] dlofProofLength, int[] resultLength);
+
+    private native long libmpecdsaSignCtxInit(int n, int t);
+
+    private native void libmpecdsaSignCtxFree(long ctx);
+
+    private native String libmpecdsaSignRound1(long ctx, String keygenResult, int[] signers,
+         int signersNum, int[] commitLength, int[] mAKLength);
+
+    private native String libmpecdsaSignRound2(long ctx, String commits, int[] commitsLength,
+         String mAKs, int[] mAKsLength, int[] mBGammaLength, int[] mBWiLength);
+
+    private native String libmpecdsaSignRound3(long ctx, String mBGammaRec, int[] mBGammaLength,
+         String mBWiRec, int[] mBWiRecLengh);
+
+    private native String libmpecdsaSignRound4(long ctx, String deltaIRec, int[] deltaILength);
+
+    private native String libmpecdsaSignRound5(long ctx, String decommitRec, int[] decommitLength,
+         int[] rDashProofLength);
+
+    private native String libmpecdsaSignRound6(long ctx, String rRec, int[] rLength,
+        String rDash, int[] rDashLength, String phase5ProofRec, int[] phase5ProofLength,
+        int[] sProofTLength);
+
+    private native String libmpecdsaSignRound7(long ctx, String sRec, int[] sLength,
+        String homoProofRec, int[] homoProofLength, String tiRec, int[] tiLength,
+        int[] sigSiLength, String message);
+
+    private native String libmpecdsaSignRound8(long ctx, String localSigRec, int[] localSigLength,
+        String siRec, int[] siLength);
 
   }
 }
